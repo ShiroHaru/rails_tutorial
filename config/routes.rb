d25cce2141700cc3ec041fbec943adf162bda924
白波瀬正晴 <Haru@shironamisemasaharu-no-iMac.local>
2019-05-24 08:00:39 +0900

    users create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :manage do
     #get 'users/new'
+    #resources :users
+    get 'signup', to: 'users#new'
+    #post '/signup', to: 'users#create'
     resources :users
+
     get 'types/new'
   end
   namespace :admin do
