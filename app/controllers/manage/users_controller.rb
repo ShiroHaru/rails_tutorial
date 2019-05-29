9930fa395ab1daf3abf6ffac230e273bfa2a7d46
白波瀬正晴 <Haru@shironamisemasaharu-no-iMac.local>
2019-05-29 16:41:04 +0900

    railsチュートリアル10まで終わり

--- app/controllers/manage/users_controller.rb
+++ app/controllers/manage/users_controller.rb
@@ -1,6 +1,7 @@
 class Manage::UsersController < Manage::ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :check_current_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -41,6 +42,12 @@ class Manage::UsersController < Manage::ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to manage_users_url
+  end
+
   private
 
     def user_params
@@ -56,9 +63,15 @@ class Manage::UsersController < Manage::ApplicationController
       end
     end
 
-  #正しいユーザーかどうか確認
+    #正しいユーザーかどうか確認
     def check_current_user
       @user = User.find(params[:id])
       redirect_to(manage_root_url) unless current_user?(@user)
     end
+
+    #管理者かどうか
+    def admin_user
+      redirect_to(admin_root_url) unless current_user.admin?
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_024949) do
+ActiveRecord::Schema.define(version: 2019_05_29_051956) do
 
   create_table "types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_05_27_024949) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
