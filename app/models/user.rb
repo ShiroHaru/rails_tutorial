bc006dfea061108a7887d9f261bdb826de93d7f3
ShiroHaru <siro.haru.w@gmail.comgit config --global user.name>
2019-05-31 15:37:37 +0900

    アカウントアクチベーションエラーの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -78,7 +78,7 @@ class User < ApplicationRecord
 
   # アカウントを有効にする
   def activate
-    update_columns(:activated, true, :activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   # 有効化用のメールを送信する
